755744d06c2f7b62e173c503bd3561fdf0bfa299
Ryan Kelley <ryankelley@Ryans-MacBook-Pro.local>
2017-04-04 18:17:17 -0400

    It Works

--- main.js
+++ main.js
@@ -2,30 +2,40 @@ var pads,
 	tampons,
 	age,
 	stateTax,
-	total;
+	taxPaid,
+	total,
+	totalPast;
 
 
 $(document).ready(function () {
 	$("#button").on("click", function (e) {
 		e.preventDefault();
-
-		var pads = parseInt($("#pads").val());
-		var tampons = parseInt($("#tampons").val());
-		var age = parseInt($("#age").val());
-		var stateTax = $("#state option:selected").data("tax");
+		//
+		pads = parseFloat($("#pads").val());
+		tampons = parseFloat($("#tampons").val());
+		age = parseFloat($("#age").val());
 
 		runMath();
 		showIt();
-		console.log
+
+	});
+
+	$("#state").on("change", function () {
+		stateTax = $("#state").find(":selected");
+		taxPaid = stateTax.data("tax");
+
 	});
 });
 
 function runMath() {
-	var total = ((pads * 5.49) + (tampons * 6.99)) * (51 - age) * stateTax
-};
+	total = ((pads * 5.49) + (tampons * 6.99)) * (51 - age) * taxPaid;
+	console.log("this is the total amount", total);
 
+};
+// shows the final tax paid
 function showIt() {
 	$("#total").text(total);
 	$("#final-answer").addClass("answer-show");
-
+	// $("#past").text(totalPast);
+	// $("#past-answer").addClass("answer-show");
 };
