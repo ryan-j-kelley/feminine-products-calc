cfea7426e60db267a82ce15d1a641e8d0b0d5ebc
Ryan Kelley <ryankelley@Ryans-MacBook-Pro.local>
2017-04-04 15:02:16 -0400

    Latest js

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-$(document).ready(function () {
-			$("#submit").on("click", function (e) {
-						e.preventDefault();
+var pads,
+	tampons,
+	age,
+	stateTax,
+	total;
 
-						var cycles = parseInt($("#cycles").val());
-						var tampons = parseInt($("#tampons").val());
-						var pads = parseInt($("#pads").val());
-						var number3 = parseInt($("#number3").val());
+
+$(document).ready(function () {
+	$("#button").on("click", function (e) {
+		e.preventDefault();
+
+		var pads = parseInt($("#pads").val());
+		var tampons = parseInt($("#tampons").val());
+		var age = parseInt($("#age").val());
+		var stateTax = $("#state option:selected").data("tax");
+
+		runMath();
+		showIt();
+		console.log
+	});
+});
+
+function runMath() {
+	var total = ((pads * 5.49) + (tampons * 6.99)) * (51 - age) * stateTax
+};
+
+function showIt() {
+	$("#total").text(total);
+	$("#final-answer").addClass("answer-show");
+
+};
